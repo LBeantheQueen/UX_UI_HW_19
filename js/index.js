40bc66d9f0bb9894b660bbc4f5fb49a9f182ade5
LBeantheQueen <lindsaybelldesign@gmail.com>
2022-06-05 18:22:19 -0400

    Scroll Function Added

---
+++ js/index.js
@@ -0,0 +1,15 @@
+console.log("Your index.js file is loaded correctly!");
+
+
+$('.navbar a').on('click', function (e) {
+ if (this.hash !== '') {
+ e.preventDefault();
+
+   const hash = this.hash;
+
+  $('html, body')
+   .animate({
+      scrollTop: $(hash).offset().top
+    },800);
+ }
+});
